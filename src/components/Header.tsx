248341d76c097300fcdb615ebb633e0d113a9af2
Simon Gustavsson <simgus1234@gmail.com>
2024-07-16 18:15:31 +0200

    Added Oslo Video

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,25 @@
 const Header = () => {
   return (
-    <header className="w-screen h-12 z-20 flex flex-col justify-center items-center absolute top-0 left-0 bg-transparent p-10">
-      <section className="flex justify-center items-center p-5 ">
-        <h1 className="text-white font-bold text-4xl">Nordisk Standard</h1>
+    <header className="w-screen h-12 z-20 flex flex-col gap-5 justify-center items-center absolute top-0 left-0 bg-transparent p-10">
+      <section className="flex justify-center items-center px-5 pt-12 w-full">
+        <h1 className="text-white font-bold text-4xl text-nowrap">
+          Nordisk Standard
+        </h1>
       </section>
       <section>
         <ul className="w-full justify-center flex items-center gap-5">
-          <li className="cursor-pointer text-white font-sans">
+          <li className="cursor-pointer text-white font-sans border-b-2 border-transparent hover:border-white transition-all">
             Erfaring Og Meriter
           </li>
-          <li className="cursor-pointer text-white font-sans">Om Oss</li>
-          <li className="cursor-pointer text-white font-sans">Tjenester</li>
-          <li className="cursor-pointer text-white font-sans">Kontakt</li>
+          <li className="cursor-pointer text-white font-sans border-b-2 border-transparent hover:border-white transition-all">
+            Om Oss
+          </li>
+          <li className="cursor-pointer text-white font-sans border-b-2 border-transparent hover:border-white transition-all">
+            Tjenester
+          </li>
+          <li className="cursor-pointer text-white font-sans border-b-2 border-transparent hover:border-white transition-all">
+            Kontakt
+          </li>
         </ul>
       </section>
     </header>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,7 +1,33 @@
+import LandingVideo from "../assets/Landing.mp4";
+
 const Hero = () => {
   return (
-    <section className="w-screen h-screen bg-[url('./assets/NorwayDesktop.jpg')] bg-cover relative">
-      <section className="w-full h-full bg-black bg-opacity-30 absolute top-0 left-0"></section>
+    <section className="w-screen h-screen relative">
+      <video
+        autoPlay
+        loop
+        muted
+        className="w-full h-full object-cover absolute top-0 left-0"
+      >
+        <source src={LandingVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <section className="w-full h-full flex justify-start items-end bg-black bg-opacity-30 absolute top-0 pb-20 p-5 md:p-10 left-0">
+        <section className=" flex flex-col gap-4 justify-start">
+          <h2 className="text-white font-bold text-3xl font-poppins md:text-6xl">
+            Nordisk Standard
+          </h2>
+          <p className="text-white font-medium texl-lg md:text-xl font-poppins md:w-4/12">
+            Vi hjelper våre boligpartnere med å øke utleieverdien ved å gi
+            enestående opplevelser til våre gjester og leietakere. Sånn at de
+            kan bruke mer tid på det de ønsker.
+          </p>
+
+          <button className="text-white bg-transparent ont-poppins backdrop-blur-sm p-4 border-2 md:w-2/12 md:hover:font-bold md:hover:scale-110 transition-all duration-300">
+            Finne ut mer
+          </button>
+        </section>
+      </section>
     </section>
   );
 };
