a57054359719e000a4b4c63c21aed34e157ded73
Simon Gustavsson <simgus1234@gmail.com>
2024-08-01 18:54:36 +0200

    Push GTAG

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -8,18 +8,26 @@ const Hero = () => {
     <section
       id="home"
       className="w-screen h-screen bg-[url('./assets/NorwayDesktop.png')] bg-cover"
+      aria-label="Home Section"
     >
       <video
         autoPlay
         controls={false}
         muted
         className="w-full h-full object-cover absolute top-0 left-0"
+        aria-label="Background Video"
       >
         <source src={LandingVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <section className="w-full h-full flex justify-center items-center md:justify-center md:items-center bg-black bg-opacity-45 absolute top-0 pb-20 p-5 md:p-10 left-0">
-        <section className="flex flex-col gap-4 md:gap-6 justify-start md:justify-center items-center">
+      <section
+        className="w-full h-full flex justify-center items-center md:justify-center md:items-center bg-black bg-opacity-45 absolute top-0 pb-20 p-5 md:p-10 left-0"
+        aria-label="Overlay Section"
+      >
+        <section
+          className="flex flex-col gap-4 md:gap-6 justify-start md:justify-center items-center"
+          aria-label="Hero Content"
+        >
           <motion.h2
             layout
             initial={{
@@ -32,6 +40,7 @@ const Hero = () => {
             }}
             transition={{ delay: 0.2, ease: "anticipate" }}
             className="text-herohead text-center font-bold text-4xl font-poppins md:text-5xl lg:text-6xl md:drop-shadow-lg"
+            aria-label="Main Heading"
           >
             Din Utleiepartner
           </motion.h2>
@@ -47,23 +56,30 @@ const Hero = () => {
             }}
             transition={{ delay: 0.3, ease: "anticipate" }}
             className="text-herosub font-medium text-center text-lg md:text-xl lg:text-2xl font-poppins"
+            aria-label="Subheading"
           >
             Vi frigjør din viktigste resurs, din tid.
           </motion.p>
           <motion.a
             href="/#reach"
-            className="w-full flex justify-center text-herosub absolute p-4  bottom-8 animate-bounce items-center md:mt-8"
+            className="w-full flex justify-center text-herosub absolute p-4 bottom-8 animate-bounce items-center md:mt-8"
+            aria-label="Scroll Down Link"
           >
-            <section className="w-12 h-12 md:w-16 md:h-16 flex justify-center items-center group bg-transparent rounded-full hover:bg-white transition-all">
+            <section
+              className="w-12 h-12 md:w-16 md:h-16 flex justify-center items-center group bg-transparent rounded-full hover:bg-white transition-all"
+              aria-label="Scroll Down Button"
+            >
               <img
                 src={Arrow}
                 alt="Arrow"
                 className="w-8 h-8 md:w-12 md:h-12 group-hover:hidden"
+                aria-hidden="true"
               />
               <img
                 src={Arrowb}
                 alt="Arrow"
                 className="w-8 h-8 md:w-12 md:h-12 group-hover:block hidden"
+                aria-hidden="true"
               />
             </section>
           </motion.a>
--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -11,63 +11,83 @@ const Services = () => {
     <section
       id="services"
       style={{ backgroundImage: `url(${Mainbg})` }}
-      className={`w-full flex flex-col justify-center items-center p-4  bg-[#f7efcf] bg-cover bg-bottom`}
+      className={`w-full flex flex-col justify-center items-center p-4 bg-[#f7efcf] bg-cover bg-bottom`}
+      aria-label="Our Services Section"
     >
-      <section className="w-full flex flex-col gap-4 md:grid grid-cols-2 py-6 md:py-12 md:w-10/12 lg:w-8/12">
+      <section
+        className="w-full flex flex-col gap-4 md:grid grid-cols-2 py-6 md:py-12 md:w-10/12 lg:w-8/12"
+        aria-label="Service Offerings"
+      >
         <GridImg
           img="https://images.unsplash.com/photo-1719579019072-96e65b294301?q=80&w=3773&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           pos="object-center"
+          aria-label="Image depicting a house for short-term rental"
         />
         <GridContent
           desc="Vi håndterer din utleie for deg og optimerer leieinntektene dine."
           title="Hva tilbyr vi?"
           href="/#services"
+          aria-label="Description of our offerings"
         />
       </section>
-      <section className="grid w-full md:w-10/12 lg:w-8/12 gap-1 md:gap-2 grid-cols-1 grid-rows-3 md:grid-cols-3 md:grid-rows-1">
+      <section
+        className="grid w-full md:w-10/12 lg:w-8/12 gap-1 md:gap-2 grid-cols-1 grid-rows-3 md:grid-cols-3 md:grid-rows-1"
+        aria-label="Service Boxes"
+      >
         <ServiceBox
-          title="Korttidsutleie
-"
+          title="Korttidsutleie"
           description="Utleie på kort sikt, enten konstant eller kun når du er på ferie eller skal bort."
           img={House}
+          aria-label="Service box for short-term rental"
         />
         <ServiceBox
           title="Langtidsutleie"
           description="Utleie for lengre perioder, som for eksempel til studenter eller fastboende."
           img={Plane}
+          aria-label="Service box for long-term rental"
         />
         <ServiceBox
           title="Kombinasjonsutleie"
           description="En fleksibel modell hvor vi kombinerer langsiktig utleie (som til studenter i høst- og vårsemesteret) og kortsiktig utleie i høysesongen"
           img={Coin}
+          aria-label="Service box for combination rental"
         />
       </section>
 
-      <section className="w-full flex flex-col gap-4 md:grid grid-cols-2 py-6 md:py-12 md:w-10/12 lg:w-8/12">
+      <section
+        className="w-full flex flex-col gap-4 md:grid grid-cols-2 py-6 md:py-12 md:w-10/12 lg:w-8/12"
+        aria-label="Contact Us Section"
+      >
         <GridContent
           desc="Vi tar oss tid til å diskutere dine ønsker og behov for å finne den beste løsningen for deg. Kontakt oss for en uforpliktende samtale"
           title="Kontakt oss"
           buttonText="Kontakt oss for en uforpliktende samtale"
-          href="/#services"
+          href="/#kontakt"
+          aria-label="Contact us for a consultation"
         />
         <GridImg
           img="https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           pos="object-center"
+          aria-label="Image depicting a consultation"
         />
       </section>
 
-      <section className="w-full flex flex-col gap-8 md:grid grid-cols-2 py-6 md:py-12 md:w-10/12 lg:w-8/12">
+      <section
+        className="w-full flex flex-col gap-8 md:grid grid-cols-2 py-6 md:py-12 md:w-10/12 lg:w-8/12"
+        aria-label="Testimonials Section"
+      >
         <GridImg
           img="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?q=80&w=3773&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           pos="object-center"
+          aria-label="Image showing happy customers"
         />
         <GridContent
           title="Fortolighet"
-          desc="Vi setter stor pris på fortroligheten vi fått fra våre partnere.Med over 100 fornøyde gjester og samarbeidspartnere jobber vi for deg og din bolig.
-"
+          desc="Vi setter stor pris på fortroligheten vi fått fra våre partnere. Med over 100 fornøyde gjester og samarbeidspartnere jobber vi for deg og din bolig."
           dataone={{ data: "Rating", value: "4.9 / 5" }}
           buttonText="Se vad partnere sier"
           href="/#meriter"
+          aria-label="Partner testimonials and ratings"
         />
       </section>
     </section>
