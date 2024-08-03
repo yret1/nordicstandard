c5fc1d0101459cc0b2063b929ea29a6c982d5399
Simon Gustavsson <simgus1234@gmail.com>
2024-08-03 16:40:03 +0200

    Push

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,30 +1,76 @@
-// import LandingVideo from "../assets/Landing.mp4";
+import LandingVideo from "../assets/Landing.mp4";
 import Arrow from "../assets/Arrowdown.svg";
 import Arrowb from "../assets/Arrowdownb.svg";
 import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 const Hero = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [playingForward, setPlayingForward] = useState(true);
+  const [intervalId, setIntervalId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    const handleEnded = () => {
+      setPlayingForward(false);
+      if (intervalId) clearInterval(intervalId);
+      const id = window.setInterval(() => {
+        if (video.currentTime <= 0) {
+          clearInterval(id);
+          setPlayingForward(true);
+          video.play();
+        } else {
+          video.currentTime -= 0.1; // Adjust this value for the reverse speed
+        }
+      }, 100); // Adjust this value for the reverse speed
+      setIntervalId(id);
+    };
+
+    const handleTimeUpdate = () => {
+      if (playingForward && video.currentTime >= video.duration) {
+        video.pause();
+        handleEnded();
+      }
+    };
+
+    video.addEventListener("ended", handleEnded);
+    video.addEventListener("timeupdate", handleTimeUpdate);
+
+    // Clean up event listeners and interval on component unmount
+    return () => {
+      video.removeEventListener("ended", handleEnded);
+      video.removeEventListener("timeupdate", handleTimeUpdate);
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, [playingForward, intervalId]);
+
   return (
     <section
       id="home"
       className="w-screen h-screen bg-[url('./assets/NorwayDesktop.png')] bg-cover"
       aria-label="Home Section"
     >
-      <img
+      {/*  <img
         src="https://media.istockphoto.com/id/1172796126/sv/foto/hamn-och-finansdistrikt-utsikt-%C3%B6ver-oslo-norge.jpg?s=2048x2048&w=is&k=20&c=xWE2i0yVRWsTiSXdjkrfL0ZUvH3lxjnVlMA7z4dpqaY="
         alt="Oslo image"
         className="w-full h-full object-cover absolute top-0 left-0"
-      />
-      {/*<video
-        autoPlay
-        controls={false}
-        muted
-        className="w-full h-full object-cover absolute top-0 left-0"
-        aria-label="Background Video"
-      >
-        <source src={LandingVideo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video> */}
+      /> */}
+      {
+        <video
+          autoPlay
+          controls={false}
+          muted
+          loop
+          ref={videoRef}
+          className="w-full h-full object-cover absolute top-0 left-0"
+          aria-label="Background Video"
+        >
+          <source src={LandingVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      }
       <section
         className="w-full h-full flex justify-center items-center md:justify-center md:items-center bg-black bg-opacity-45 absolute top-0 pb-20 p-5 md:p-10 left-0"
         aria-label="Overlay Section"
