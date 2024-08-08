653ce0a7eaea5a16bd37edcdfd55244f9d32263d
Simon Gustavsson <simgus1234@gmail.com>
2024-08-08 13:13:55 +0200

    Added reviews

--- src/sections/Socialproof.tsx
+++ src/sections/Socialproof.tsx
@@ -27,32 +27,39 @@ const Socialproof = () => {
 
   const reviewsGuests = [
     {
-      name: "Morten Larsen",
+      name: "Todd",
       rating: 5,
 
       img: "https://images.unsplash.com/photo-1578463637591-54d8030ba6c7?q=80&w=3864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       review:
-        "Vi hadde en herlig helg her. Boligen var moderne og godt utstyrt, med alt vi trengte. Verten var svært imøtekommende og ga oss gode tips til lokale restauranter og severdigheter. Anbefales på det sterkeste!",
+        "This place was fantastic and Tessie and Samir were the 2 best hosts I've ever had!!! I highly recommend their place!! We had a GREAT time and would definitely stay there again!",
     },
     {
-      name: "Ingrid Olsen",
+      name: "Tess Erngren",
       rating: 5,
 
       img: "https://plus.unsplash.com/premium_photo-1670606249973-9cd2f650ad4a?q=80&w=3334&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       review:
-        "Dette var en av de beste Airbnb-opplevelsene vi har hatt. Rommet var romslig og komfortabelt, og vi følte oss hjemme med en gang. Vertskapet var fantastisk – de sørget for at vi hadde alt vi trengte. Tusen takk!",
+        "I wish we got to hang out with the hosts! Just from the short messages, books and interior i figure these are people I'd like to hang out with. Great apartment, great location and great hosts!",
     },
     {
-      name: "Torvald Svendsen",
+      name: "Margrete",
       rating: 5,
 
       img: "https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?q=80&w=3246&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       review:
-        "En flott opplevelse fra start til slutt! Leiligheten var både stilfull og funksjonell, og vi satte stor pris på de små detaljene som gjorde oppholdet spesielt. Verten var vennlig og tilgjengelig for spørsmål. Vi håper å komme tilbake snart!",
+        "Vi hadde noen veldig fine dager i leiligheten til Tessie og Samir. Leiligheten lå flott til, med gangavstand til hyggelige områder rundt Alexander Kiellans plass, og god tilgang på kollektivtransport med til sentrum. Selve leiligheten var både fin og koselig innredet, og perfekt i størrelse til vår familie på fire. Tessie og Samir har vært veldig både ryddige og lette å kommunisere med, og de har lagt alt til rette for et flott opphold i Oslo for oss. Vi kan virkelig anbefale stedet videre! Og bi kommer gjerne igjen :)",
+    },
+    {
+      name: "Kevin",
+      rating: 5,
+      img: "https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?q=80&w=3246&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      review:
+        "Perfectly located and very charming home for a short or longer stay with a small group. The hosts were very friendly, helpful and easy to communicate with. This AirBNB is perfectly positioned between the cool neighborhood Grünerløkka whilst also being a short walk away from both Oslo's main train station and the city center. The place is also conveniently located near resturants, supermarkets and a new food hall. We stayed in the home for a weekend in summer, but i can imagine that this traditional wooden home would also be lovley to stay at during colder winter months. Would higly recommend!",
     },
   ];
 
-  const reviews = [
+  /*const reviews = [
     {
       name: "Lise Hansen",
       title: "CEO",
@@ -77,7 +84,7 @@ const Socialproof = () => {
       review:
         "Å jobbe med Nordisk Standard har vært en veldig positiv opplevelse. De har gjort en utmerket jobb med å leie ut vår eiendom, og kommunikasjonen har vært topp. Vi er veldig glade for å ha dem som vår partner.",
     },
-  ];
+  ];//*/
   return (
     <section
       id="meriter"
@@ -101,7 +108,7 @@ const Socialproof = () => {
         />
       </section>
       <Reach />
-      <p className="text-contact font-comorant font-bold py-10 md:text-4xl text-2xl lg:text-5xl">
+      {/*<p className="text-contact font-comorant font-bold py-10 md:text-4xl text-2xl lg:text-5xl">
         Se hva våre partnere sier!
       </p>
       <section className="w-screen md:w-12/12 lg:w-11/12 xl:w-10/12 2xl:w-8/12 py-12 grid place-items-center justify-items-center grid-cols-1 md:grid-cols-1 lg:grid-cols-3 p-2 md:p-4 lg:p-6 gap-2 md:gap-4 lg:gap-6">
@@ -116,11 +123,11 @@ const Socialproof = () => {
           />
         ))}
       </section>
-
+*/}
       <p className="text-contact font-comorant font-bold py-10 md:text-4xl text-2xl lg:text-5xl">
         Se hva våre gjester sier!
       </p>
-      <section className="w-screen md:w-12/12 lg:w-11/12 xl:w-10/12 2xl:w-8/12 py-12 grid place-items-center justify-items-center grid-cols-1 md:grid-cols-1  lg:grid-cols-3 p-2 md:p-4 lg:p-6 gap-2 md:gap-4 lg:gap-6">
+      <section className="w-screen md:w-12/12 lg:w-11/12 xl:w-10/12 2xl:w-8/12 py-12 grid place-items-center justify-items-center grid-cols-1 md:grid-cols-1  lg:grid-cols-2 p-2 md:p-4 lg:p-6 gap-2 md:gap-4 lg:gap-6">
         {reviewsGuests.slice(0, numReviews).map((review, index) => (
           <SocialCard
             key={index + review.name}
